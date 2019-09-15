0a10a158a9836ada9ce9ed57b4e7f8b16d9548de
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 14:41:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,20 @@
 def hello_t(array)
   
-  i = 0 
+  if block_given?
   
-  while i < array.length
-    yield array[i]
-    i += 1
-  end 
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
   
-  array 
+  else
+    puts "Hey! No block was given!"
+  
+  end
   
 end
 
